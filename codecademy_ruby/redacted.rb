bc8534811acfc6f0d5d83463f1abae76da50f453
atxrenegade <HarleighAbel@gmail.com>
2017-01-27 22:09:52 -0700

    first commit

---
+++ codecademy_ruby/redacted.rb
@@ -0,0 +1,23 @@
+=begin
+Harleigh Abel 
+Jan. 27th, 2016
+Ruby Exercise
+Codecademy
+"Redacted"
+=end
+
+puts "Please enter your message:"
+message = gets.chomp
+puts "What word would you like to redact?"
+text = gets.chomp
+
+words = message.split (" ")
+
+words.each do |letter|
+    if letter == text
+        print " REDACTED "
+    else
+        print " #{letter} "
+    end    
+end    
+    
